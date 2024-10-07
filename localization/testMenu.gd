60e053c9e01a78bd75735f4d315f61e5cd3f72c3
njj-abs <njj@abstractit.in>
2024-10-07 16:15:58 +0530

    Modified the localization.

--- localization/testMenu.gd
+++ localization/testMenu.gd
@@ -1,12 +1,22 @@
 extends Control
 
+var template = {
+	"baseId": "hello",
+	"placeholders": {
+		"ph": {
+			"en": "back",
+			"es": "front"
+		}
+	}
+}
+
 func _ready():
-	$CenterContainer/GridContainer/Button.text = tr("hello {ph}").format({"ph": " njj"})
-	pass
+	$CenterContainer/GridContainer/Button.text = tr("hello").format({"ph": " njj"})
 
 func _on_button_pressed():
 	if TranslationServer.get_locale() == "en":
 		TranslationServer.set_locale("es")
+		#$CenterContainer/GridContainer/Button.text = tr("hello").format({"ph": " back"})
 	else:
 		TranslationServer.set_locale("en")
-	
+		#$CenterContainer/GridContainer/Button.text = tr("hello").format({"ph": " front"})
