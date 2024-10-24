9d1a0d2542bc52962b2d634bd5af7c3bcdcfa407
njj-abs <njj@abstractit.in>
2024-10-24 16:08:53 +0530

    Improved the ripple effect.

--- effects/ripple/Ripple.gd
+++ effects/ripple/Ripple.gd
@@ -1,132 +1,65 @@
-#extends TextureRect
-#
-#var shader_material: ShaderMaterial
-#
-#func _ready():
-	## Assign the shader material to the TextureRect
-	#shader_material = material
-#
-#func _input(event):
-	#if event is InputEventScreenTouch or event is InputEventMouseButton:
-		#if event.pressed:
-			#var touch_position = event.position / size
-			#shader_material.set_shader_parameter("ripple_center", touch_position)
-			#shader_material.set_shader_parameter("ripple_radius", 0.0) # Start with radius 0
-			#_start_ripple_effect()
-#
-#func _start_ripple_effect():
-	## Create a single tween for ripple expansion
-	#var tween = create_tween()
-	#
-	## Animate ripple radius from 0 to 1 (expansion) over 1 second
-	#tween.tween_property(shader_material, "shader_parameter/ripple_radius", 1.0, 1.0)
-	#
-	## Optionally, connect the tween completion to hide/reset the ripple
-	#await tween.finished
-	#_on_ripple_complete()
-#
-#func _on_ripple_complete():
-	## After the ripple effect completes, reset or hide the ripple
-	#shader_material.set_shader_parameter("ripple_radius", 0.0)
-
-#func _process(delta):
-	## Update custom time manually, for example, increase it by delta to progress
-	#
-	## If you want to reset or manipulate time, modify custom_time_value accordingly
-	## Example of pausing time:
-	#if custom_time_values > 5:
-		#material.set_shader_parameter("custom_time", custom_time_values)
-#
-	## Example of reversing time:
-	## custom_time_value -= delta
-#
-	## Set the custom time in the shader
-	#else:
-		#custom_time_values += delta
-		#material.set_shader_parameter("custom_time", custom_time_values)
-
-#func _input(event):
-	#if event is InputEventScreenTouch and event.pressed:
-		#var ripple_size = shader_material.get_shader_parameter("size")
-#
-		#var uv_position = Vector2(
-			#event.position.x / size.x,
-			#event.position.y / size.y
-		#)
-#
-		#uv_position *= ripple_size
-		#uv_positions.append(uv_position)
-		#startTimes.append(0)
-		#endTimes.append(1)
-		#var ticks_in_seconds = Time.get_ticks_msec() / 1000.0
-		#startTimes.append(ticks_in_seconds)
-		#endTimes.append(ticks_in_seconds+10)
-		##var touch_positions:PackedColorArray = [Vector4(uv_position.x,uv_position.y,0,1)]
-		##shader_material.set_shader_parameter("touch_position", uv_position)
-		##shader_material.set_shader_parameter("touch_position", touch_positions)
-		#shader_material.set_shader_parameter("touch_positions", uv_positions)
-		##shader_material.set_shader_parameter("touch_position_y", uv_positions_y)
-		#shader_material.set_shader_parameter("start_time", startTimes)
-		#shader_material.set_shader_parameter("end_time", endTimes)
-
 extends TextureRect
 
-var shader_material
-var uv_positions:Array = []
-var startTimes:Array = []
-var endTimes:Array = []
-var touch_positions = [Vector2(0, 0), Vector2(0, 0)]
+var shader_material: ShaderMaterial
 var custom_time_values = []
+var touch_positions = [Vector2(0, 0), Vector2(0, 0)]
 var active_touches = 0
 var ripple_duration = 5.0
+var max_ripples = 2
 
 func _ready():
-	var array = []
-	print(array.is_empty())
 	shader_material = material as ShaderMaterial
-	shader_material.set_shader_parameter("custom_time", custom_time_values)
+	_update_shader_params()
 
 func _process(delta):
+	# Use a separate array to mark indices for removal
+	var remove_indices = []
+
 	# Update custom times for active ripples
 	for i in range(active_touches):
-		# Only update if time is less than the ripple duration
 		if custom_time_values[i] < ripple_duration:
-			# Increment the custom time until it reaches the duration limit
 			custom_time_values[i] += delta
-		# Clamp the time value to ensure it doesn't exceed ripple_duration
-		custom_time_values[i] = min(custom_time_values[i], ripple_duration)
+		else:
+			# Mark ripples that have exceeded their time limit
+			remove_indices.append(i)
 
-		if custom_time_values[i] >= ripple_duration:
-			custom_time_values.remove_at(i)
-			active_touches -= 1
+	# Remove ripples that have completed their animation
+	# Iterate backwards to avoid issues with index shifting
+	for i in remove_indices.size():
+		custom_time_values.remove_at(i)
+		touch_positions.remove_at(i)
+		active_touches -= 1
 
 	# Update the shader with the new custom times
 	_update_shader_params()
 
-
 func _input(event):
-	# If a touch event or mouse button event happens, update the touch position and reset custom time
-
 	if event is InputEventScreenTouch and event.pressed:
-		if active_touches < 2:
+		if active_touches < max_ripples:
 			var ripple_size = shader_material.get_shader_parameter("size")
 			var uv_position = Vector2(
-			event.position.x / size.x,
-			event.position.y / size.y
-		)
+				event.position.x / size.x,
+				event.position.y / size.y
+			)
 
 			uv_position *= ripple_size
-			touch_positions[active_touches] = uv_position
-
-			if custom_time_values.size() > active_touches:
-				custom_time_values[active_touches] = 0.0  # Reset time for the new touch point
+			if active_touches >= touch_positions.size():
+				touch_positions.append(uv_position)
 			else:
+				touch_positions[active_touches] = uv_position
+
+			# Reset the ripple time for this touch
+			if active_touches >= custom_time_values.size():
 				custom_time_values.append(0.0)
+			else:
+				custom_time_values[active_touches] = 0.0
+
+			# Increment active touches
 			active_touches += 1
 
-	_update_shader_params()
+			# Immediately update the shader after a new touch event
+			_update_shader_params()
 
 func _update_shader_params():
-	# Update shader params for custom times and touch positions
-	material.set_shader_parameter("custom_times", custom_time_values)
-	material.set_shader_parameter("touch_positions", touch_positions)
+	shader_material.set_shader_parameter("custom_times", custom_time_values)
+	shader_material.set_shader_parameter("touch_positions", touch_positions)
