6fa46e1ea1abae18cc79fca65845e26f38a12daa
njj-abs <njj@abstractit.in>
2024-11-15 16:32:54 +0530

    Refactored firework effect: moved color selection to fireWork.gd, updated rocket movement and emission, and added color to explosion and spark.

--- effects/fireWorks/fireWork.gd
+++ effects/fireWorks/fireWork.gd
@@ -4,9 +4,17 @@ const explosionScene = preload("res://effects/fireWorks/explosion.tscn")
 const rocketScene = preload("res://effects/fireWorks/rocket.tscn")
 const sparkScene = preload("res://effects/fireWorks/spark.tscn")
 
+var colors = [
+	Color("red"),
+	Color("green"),
+	Color("yellow"),
+]
+
 func _on_button_pressed():
+	var color = colors[randi_range(0, colors.size() - 1)]
 	var rocket = rocketScene.instantiate()
 	add_child(rocket)
+	rocket.color = color
 	await rocket.play()
 	var explosion = explosionScene.instantiate()
 	var spark = sparkScene.instantiate()
@@ -14,11 +22,17 @@ func _on_button_pressed():
 	add_child(spark)
 	explosion.position = rocket.position
 	spark.position = rocket.position
+	spark.color = color
+	explosion.color = color
 	spark.emitting = true
 	explosion.emitting = true
-	rocket.queue_free()
-	spark.finished.connect(_onSparkFinished.bind(spark))
-	explosion.finished.connect(_onSparkFinished.bind(explosion))
+	rocket.emitting = false
+	spark.finished.connect(_onSparkFinished.bind(spark, rocket))
+	explosion.finished.connect(_onExplosionFinished.bind(explosion))
 
-func _onSparkFinished(node):
+func _onSparkFinished(node, rocket):
+	rocket.queue_free()
+	node.queue_free()
+
+func _onExplosionFinished(node):
 	node.queue_free()
--- effects/fireWorks/rocket.gd
+++ effects/fireWorks/rocket.gd
@@ -1,19 +1,12 @@
 extends CPUParticles2D
 
-var colors = [
-	Color("red"),
-	Color("green"),
-	Color("yellow"),
-]
-
 func play():
-	color = colors[randi_range(0, colors.size() - 1)]
 	var screen = get_viewport_rect().size
 	var tween = get_tree().create_tween()
 	var initialX = randi_range(100, screen.x-100)
-	var endX = randi_range(initialX, initialX + 50)
+	var endX = randi_range(initialX - 50, initialX + 50)
 	var endy = randi_range(screen.y/2, 100)
-	tween.tween_method(_setRocketPosition, Vector2(initialX, screen.y), Vector2(endX, endy), 1)
+	tween.tween_method(_setRocketPosition, Vector2(initialX, screen.y), Vector2(endX, endy), 3)
 	await tween.finished
 
 func _setRocketPosition(value):
